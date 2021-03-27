569e75fd5f37c90e45e581bce3f00ed1c4429cdb
Sean <Stp29@users.noreply.github.com>
2021-03-27 18:28:29 -0500

    dsfas

--- Main.py
+++ Main.py
@@ -4,37 +4,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
 
-daysToHoldStock = 14
 name = 'DVAX'
+startDate = '2020-01-01'
+endDate = '2021-03-26'
 
-ticker = yfinance.Ticker(name)
-data = ticker.history(period="max", interval="1d")
-data['Forward Close'] = data['Close'].shift(-daysToHoldStock)
-data['Forward Return'] = (data['Forward Close'] - data['Close'])/data['Close']
+printResult = []
+for daysToHoldStock in range(1,200):
 
-result = []
-train_size = 0.6
-for sma_length in range(20, 500):
-    data['SMA'] = data['Close'].rolling(sma_length).mean()
-    data['input'] = [int(x) for x in data['Close'] > data['SMA']]
+    ticker = yfinance.Ticker(name)
+    data = ticker.history(interval="1d", start=startDate, end=endDate)
+    data['Forward Close'] = data['Close'].shift(-daysToHoldStock)
+    data['Forward Return'] = (data['Forward Close'] - data['Close'])/data['Close']
 
-    df = data.dropna()
-    training = df.head(int(train_size * df.shape[0]))
-    test = df.tail(int((1 - train_size) * df.shape[0]))
+    result = []
+    train_size = 0.6
+    for sma_length in range(20, 500):
+        data['SMA'] = data['Close'].rolling(sma_length).mean()
+        data['input'] = [int(x) for x in data['Close'] > data['SMA']]
 
-    tr_returns = training[training['input'] == 1]['Forward Return']
-    test_returns = test[test['input'] == 1]['Forward Return']
-    mean_forward_return_training = tr_returns.mean()
-    mean_forward_return_test = test_returns.mean()
-    pvalue = ttest_ind(tr_returns, test_returns, equal_var=False)[1]
+        df = data.dropna()
+        training = df.head(int(train_size * df.shape[0]))
+        test = df.tail(int((1 - train_size) * df.shape[0]))
 
-    result.append({
-        'sma_length': sma_length,
-        'training_forward_return': mean_forward_return_training,
-        'test_forward_return': mean_forward_return_test,
-        'p-value': pvalue
-    })
+        tr_returns = training[training['input'] == 1]['Forward Return']
+        test_returns = test[test['input'] == 1]['Forward Return']
+        mean_forward_return_training = tr_returns.mean()
+        mean_forward_return_test = test_returns.mean()
+        pvalue = ttest_ind(tr_returns, test_returns, equal_var=False)[1]
 
-result.sort(key = lambda x : -x['training_forward_return'])
+        result.append({
+            'days-to-hold': daysToHoldStock,
+            'sma_length': sma_length,
+            'training_forward_return': mean_forward_return_training,
+            'test_forward_return': mean_forward_return_test,
+            'p-value': pvalue
+        })
 
-print(result[0])
+    result.sort(key = lambda x : -x['training_forward_return'])
+
+    printResult.append(result[0])
+
+for p in printResult:
+    print(p)
